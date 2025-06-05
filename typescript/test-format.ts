8cca6f2f6e302f09a9044ebddbde95c9528803fb
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2025-06-05 14:56:35 +0200

    Test prettier commit hook formatting

--- typescript/test-format.ts
+++ typescript/test-format.ts
@@ -1,4 +1,7 @@
 // Test file for prettier formatting
-const test={a:1,b:2,c:3};
-function badFormat(x:number,y:string){return x+y;}
+const test = { a: 1, b: 2, c: 3, d: 4 };
+function badFormat(x: number, y: string) {
+  return x + y;
+}
+const another = { badly: "formatted" };
 export default test;
