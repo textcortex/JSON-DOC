4b07c2301f9ee0c8472af196aaaa718d48d3a2a0
Abreham Gezahegn <abreham0913@gmail.com>
2025-06-24 20:55:52 +0300

    checkpoint

--- typescript/vitest.config.mjs
+++ typescript/vitest.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
